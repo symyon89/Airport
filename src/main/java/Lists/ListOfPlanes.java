26e79c331f1c4d496a0f140f40d3ccb10fae2126
George <symyon89@yahoo.com>
2021-11-12 20:23:29 +0200

    Start Airline Project

--- src/main/java/Lists/ListOfPlanes.java
+++ src/main/java/Lists/ListOfPlanes.java
@@ -1,4 +1,43 @@
 package Lists;
 
+import MainClasses.Plane;
+import MainClasses.ReadFiles;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ListOfPlanes {
+    private Map<String, Plane> planes;
+    private final AtomicInteger index = new AtomicInteger();
+    Scanner scannerText = new Scanner(System.in);
+    Scanner scannerNumber = new Scanner(System.in);
+
+    public ListOfPlanes() {
+        Runnable readPlanes = () -> planes = ReadFiles.readPlanes();
+        Thread readFilePlanes = new Thread(readPlanes);
+        readFilePlanes.start();
+        try {
+            readFilePlanes.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showPlanes() {
+
+        index.set(1);
+        planes.forEach((key, plane) -> System.out.println(index.getAndIncrement() + "." + plane ));
+
+    }
+    public void addPlane(){
+        Plane plane = new Plane();
+        System.out.println("Enter plane name : ");
+        plane.setPlaneName(scannerText.nextLine());
+        System.out.println("Enter plane average speed : ");
+        plane.setAverageSpeed(scannerNumber.nextInt());
+        System.out.println("Enter number of seats : ");
+        plane.setNumebrOfSeats(scannerNumber.nextInt());
+        planes.put(plane.getPlaneName(),plane);
+        ReadFiles.updatePlanes(planes);
+    }
 }
--- src/main/java/Menu/PlaneMenu.java
+++ src/main/java/Menu/PlaneMenu.java
@@ -1,4 +1,39 @@
 package Menu;
 
+import Lists.ListOfPlanes;
+
+import java.util.Scanner;
+
 public class PlaneMenu {
+    ListOfPlanes planes = new ListOfPlanes();
+    Scanner scannerNumber = new Scanner(System.in);
+    private void mainMenu(){
+        System.out.println("1.Show Planes");
+        System.out.println("2.Add Plane");
+        System.out.println("3.Update Plane");
+        System.out.println("4.Delete Plane");
+        System.out.println("5.Search by plane name");
+        System.out.println("0.Exit");
+        System.out.print("Choose option :");
+    }
+
+    public void menu(){
+        byte menuOption;
+        do {
+            mainMenu();
+            menuOption = scannerNumber.nextByte();
+            menuAction(menuOption);
+        }while (menuOption != 0);
+    }
+    private void menuAction(byte menuOption) {
+        switch (menuOption){
+            case 1 -> planes.showPlanes();
+            case 2 -> planes.addPlane();
+            case 3 -> planes.showPlanes();
+            case 4 -> planes.showPlanes();
+            case 5 -> planes.showPlanes();
+            case 0 -> System.out.println("Goodbye!");
+            default -> System.out.println("Invalid option");
+        }
+    }
 }
