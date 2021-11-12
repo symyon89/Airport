e9d97e6a9a93fb829b59c5d7d197ac9eba1d9afc
George <symyon89@yahoo.com>
2021-11-12 21:01:21 +0200

    Start Airline Project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import Menu.FlightMenu;
+import Menu.PlaneMenu;
 
 public class Main {
     public static void main(String[] args) {
-        FlightMenu flightMenu = new FlightMenu();
-        flightMenu.menu();
+//        FlightMenu flightMenu = new FlightMenu();
+//        flightMenu.menu();
+        PlaneMenu planeMenu = new PlaneMenu();
+        planeMenu.menu();
     }
 }
