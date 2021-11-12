4d5a29c07c9a1ff5a75cab7ef430e62a70246d23
George <symyon89@yahoo.com>
2021-11-12 10:48:48 +0200

    Start Airline Project

--- src/main/java/Lists/ListOfFlights.java
+++ src/main/java/Lists/ListOfFlights.java
@@ -10,14 +10,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ListOfFlights {
 
-    private final Map<String, Flight> flights;
-    private final Map<String, Plane> planes;
+    private Map<String, Flight> flights;
+    private Map<String, Plane> planes;
     TravelTime travelHours = (dist, avg) -> (dist / avg);
     TravelTime travelminutes = (dist, avg) -> (int) (((dist / (avg * 1.0)) - ((dist / avg))) * 60);
 
     public ListOfFlights() {
-        flights = ReadFiles.readFlights();
-        planes = ReadFiles.readPlanes();
+        Runnable readFlights = () -> flights = ReadFiles.readFlights();
+        Runnable readPlanes = () -> planes = ReadFiles.readPlanes();
+        Thread readFileFlights = new Thread(readFlights);
+        Thread readFilePlanes = new Thread(readPlanes);
+        readFileFlights.start();
+        readFilePlanes.start();
+        try {
+            readFileFlights.join();
+            readFilePlanes.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -25,7 +35,7 @@ public class ListOfFlights {
         AtomicInteger index = new AtomicInteger();
         index.set(1);
 
-        flights.forEach((key, flight) -> System.out.println(index.getAndIncrement() + "." + flight + "Travel time = "
+        flights.forEach((key, flight) -> System.out.println(index.getAndIncrement() + "." + flight + " Travel time = "
                 + travelHours.calculateTime(flight.getDistance(),planes.get(flight.getPlane()).getAverageSpeed()) + " hours "
                 + travelminutes.calculateTime(flight.getDistance(),planes.get(flight.getPlane()).getAverageSpeed()) + " minutes"));
     }
